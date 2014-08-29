0b6f5d35fdfa011acb8de6b5b5ab826415b49552
helloiloveit <hello.i.love.it@gmail.com>
2014-08-29 11:16:20 +0700

    update
    update

---
+++ chapter2/Huy/exeo3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int countString(string str, char countChar)
+{
+    int countSpace = 0;
+    for (int i =0; i < str.length(); i++)
+        if(str[i] == countChar)
+            countSpace ++;
+    return countSpace;
+}
+
+
+int main() {
+    ifstream in("exeo2.cpp"); // Open for reading
+    ofstream out("temp.cpp"); // Open for writing
+    string s;
+    int totalCount = 0;
+    while(getline(in, s)){ // Discards newline char
+        int rst =countString(s, ' ');
+        //cout <<"space = "<<rst<<endl;
+        totalCount = totalCount + rst;
+        
+    }
+    cout <<"result is = "<<totalCount<<endl;
+} ///:~
---
+++ chapter2/Huy/helloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout << "my dog name is  " << "jinny" << "!!" << endl;
+}
